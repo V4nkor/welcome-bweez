54bcf62c6255615367eacc8da7082468c17f1a4d
Mathieu Morgat <vanknpix@gmail.com>
2023-07-10 03:03:59 +0200

    Stable commit before improvements

--- src/components/WelcomeBweez.vue
+++ src/components/WelcomeBweez.vue
@@ -4,8 +4,9 @@
     </video>
     <div ref="page1" :class="`center transition page1 ${index === 1 ? 'show' : 'hidden'}`">
         <div @click="nextPage" class="clickable content">
-            <h1>Hello world !</h1>
-            Ce site est un exercice d'apprentissage de vue.js dans le cadre d'un entretien d'alternance pour mon année 2023-2025 en Master Développement chez Sup de Vinci<br>
+            <h1><span :class="`${terminal === 1 ? 'terminal' : 'notTerminal'}`">></span>Hello world !</h1>
+            Ce site est un exercice d'apprentissage de vue.js dans le cadre d'un entretient d'alternance pour mon année 2023-2025 en Master Développement chez Sup de Vinci<br><br>
+            <span>>Click Me !&lt;</span>
         </div>
     </div>
     <div ref="page2" :class="`center transition page2 ${index === 2 ? 'show' : 'hidden'}`">
@@ -13,19 +14,21 @@
             <h1>Qui suis-je ?</h1>
             <img src="images/mathieu.png">
             <p>
-                Mathieu MORGAT, etudiant à l'IUT d'Orsay en Licence Professionelle PRISM, je souhaite poursuivre mes études en informatique à Sup de Vinci pour un master de deux ans.
-                <br>
-                Vous pourrez accéder à mon portfolio web via ce lien : <a target="blank" href="https://mmorgat.ydns.eu">Portfolio Web</a><br>
+                Mathieu MORGAT, etudiant à l'IUT d'Orsay en Licence Professionelle PRISM. <br>
+                Je souhaite poursuivre mes études en informatique à Sup de Vinci pour un master de deux ans.
+                <br><br>
+                Vous pourrez accéder à mon portfolio : <a target="blank" href="https://mmorgat.ydns.eu">Portfolio Web</a><br><br>
+                Mon CV Sup de Vinci: <a target="blank" href="PDF/cv-mathieu-morgat.pdf">CV</a><br><br>
                 Ainsi qu'à mon github : <a target="blank" href="https://github.com/V4nkor">Profil Github</a> <br>
             </p>
-            <span @click="nextPage" class="clickable bigArrow">&rarr;</span>
+            <span @click="previousPage" class="clickable bigArrow">	&larrhk;</span><span @click="nextPage" class="clickable bigArrow">&rarrhk;</span>
         </div>
     </div>
     <div ref="page3" :class="`center transition page3 ${index === 3 ? 'show' : 'hidden'}`">
         <div class="content">
             <div class="scrollable">
                 <h1>Projets personnels :</h1><br>
-                En dehors des cours et de mes expériences professionelles, j'ai pu effectuer des projets personnels pour apprendre divers aspects de l'informatique et expérimenter.<br><br>
+                En dehors des cours et de mes expériences professionelles, j'ai pu effectuer des projets personnels pour apprendre divers aspects de l'informatique et expérimenter : <br><br>
                 <div class="elementGrid">
                     <div class="elementDiv">
                         <img class="elementImage" src="/images/piweb.jpg">
@@ -36,33 +39,27 @@
                     <div class="elementDiv">
                         <img class="elementImage" src="/images/odroid.jpg">
                         <h4>NAS Odroid HC4</h4>
-                        <p>Projet IOS de jeu du Pendu</p>
-                        <span>Status : En cours d'utilisation</span><br>
+                        <p>Serveur de stockage NAS sous openmediavault créé sur une Odroid HC4 sous armbian</p>
+                        <span>Status : En cours d'utilisation</span>
                     </div>
-                    <!-- <div class="elementDiv">
-                        <img class="elementImage" src="/images/gapm.png">
-                        <h4>Projet Cristo</h4>
-                        <p>Projet de reconnaissance vocale en IHM</p>
-                        <span>Status : Ebauche - Non hébergée</span>
-                    </div> -->
                 </div><br>
                 <h1>Exercices Scolaires</h1><br>
                 Lors de ma licence professionelle, j'ai pu à de nombreuses reprises participer 
-                à des projets en groupes ou seul de développement web ou mobile, en voici quelques uns : <br><br>
+                à des projets de développement web ou mobileen groupes ou seul, en voici quelques uns : <br><br>
                 <div class="elementGrid">
                     <div class="elementDiv">
-                        <img class="elementImage" src="/images/gapm.png">
+                        <img class="elementImage" src="/images/gapm.png"><br><br>
                         <h4>Projet GAPM</h4>
-                        <p>Projet de site de RDV médicaux</p>
+                        <p>Projet de fin d'année. Site de RDV médicaux</p>
                         <span>Status : Finis - Hébergé Privé</span><br>
                         <a target="blank" href="PDF/GAPM - Cahier des charges.pdf">Rapport de rendu</a>
                     </div>
                     <div class="elementDiv">
-                        <img class="elementImage" src="/images/placeholder.png">
+                        <img class="elementImage" src="/images/pendix.png">
                         <h4>Projet Pendix</h4>
                         <p>Projet IOS de jeu du Pendu</p>
                         <span>Status : Finis</span><br>
-                        <a target="blank" href="PDF/Pendix.pdf">Rapport de rendu</a>
+                        <a target="blank" href="PDF/DOC IOS.pdf">Rapport de rendu</a>
                     </div>
                     <div class="elementDiv">
                         <img class="elementImage" src="/images/cristo.png">
@@ -72,7 +69,7 @@
                         <a target="blank" href="PDF/Cristo.pdf">Rapport de rendu</a>
                     </div>
                     <div class="elementDiv">
-                        <img class="elementImage" src="/images/placeholder.png">
+                        <img class="elementImage" src="/images/webpizza.png">
                         <h4>TP webpizza</h4>
                         <p>Projet sous DJango de pizza</p>
                         <span>Status : Ebauche - <a target="blank" href="http://mmorgat.projects.ydns.eu:3000/">Hébergé</a></span>
@@ -82,20 +79,20 @@
                 En tant que stagiaire puis alternant, j'ai travaillé sur des projets ancrés dans le monde professionnel : <br><br>
                 <div class="elementGrid">
                     <div class="elementDiv">
-                        <img class="elementImage" src="/images/placeholder.png">
+                        <img class="elementImage" src="/images/dtp-ged.png">
                         <h4>Les Archives BTP</h4>
                         <p>Armoire à plan</p>
                         <span>Status : <a target="blank" href="https://dtp-ged.fr">Production - Privé</a></span>
                     </div>
                     <div class="elementDiv">
-                        <img class="elementImage" src="/images/placeholder.png">
+                        <img class="elementImage" src="/images/gallimedia.png">
                         <h4>Gallimedia - Scripts Bash</h4>
-                        <p>Scripts bash d'automatisation de mise en place de serveurs webs et Drupal, migration de bases de données et création de documentation</p>
+                        <p>Scripts bash d'automatisation de mise en place de serveurs webs et Drupal sous linux, migration de bases de données et création de documentation</p>
                         <span>Status : Finis</span>
                     </div>
                 </div><br>
             </div>
-            <span @click="nextPage" class="clickable bigArrow">&rarr;</span>
+            <span @click="previousPage" class="clickable bigArrow">	&larrhk;</span><span @click="nextPage" class="clickable bigArrow">&rarrlp;</span>
         </div>
     </div>
     <span class="pagination">{{index}}/3</span>
@@ -113,12 +110,29 @@ const page3:Ref<HTMLElement | undefined> = ref(); */
 
 const backgroundVideo:Ref<HTMLElement | undefined> = ref();
 
+const terminal = ref(1);
+
 /* const pages:Array<Ref<HTMLElement | undefined>> = [page1,page2,page3]; */ //Array contenant toutes les pages
 
 function nextPage(){
     index.value ++;
     if(index.value > 3){
         index.value = 1;
+    }
+}
+function previousPage(){
+    index.value --;
+    if(index.value <= 0){
+        index.value = 3;
+    }
+}
+
+function showHideTerminal(){
+    if(terminal.value === 1){
+        terminal.value = 0;
+    }
+    else{
+        terminal.value = 1;
     }
 }
 
@@ -133,6 +147,7 @@ onMounted(() => {
     else{
 
     }
+    setInterval(showHideTerminal, 2000);
 });
 
 </script>
@@ -145,6 +160,10 @@ $dark-green: #002000;
 .bigArrow{
     font-size: 30px;
     font-weight: bolder;
+}
+.bigArrow:hover{
+    cursor:pointer;
+    font-size: 34px;
 }
 
 .content{
@@ -167,12 +186,14 @@ h1{
 .page1{
     .content:hover{
         cursor:pointer;
+        transform: scale(1.05);
     }
 }
 
 .page2{
     img{
         width:200px;
+        border-radius: 25px;
     }
 }
 
@@ -214,11 +235,10 @@ h1{
 
 .elementImage:hover {
     transform: scale(1.05);
-    /* cursor: pointer; */
 }
 
 .transition{
-    transition: all 1s;
+    transition: all 0.9s;
 }
 
 .pagination{
@@ -228,6 +248,7 @@ h1{
     right: 10px;
     color: $main-green;
     background-color: black;
+    opacity:0.9;
     padding:10px;
     border-radius: 15px;
 }
@@ -276,16 +297,24 @@ h1{
 
 .show{
     display:flex;
-    opacity:1;
+    opacity:0.9;
     animation-duration: 2s;
-    animation-name: showAnimation
+    animation-name: showAnimation;
+    img{
+        opacity:1;
+    }
 }
 
 .hidden{
     display:none;
     opacity:0;
     animation-duration: 2s;
-    animation-name: hiddenAnimation
+    animation-name: hiddenAnimation;
+}
+
+.notTerminal{
+    display:none;
+    opacity:0;
 }
 
 @keyframes showAnimation {
@@ -295,14 +324,14 @@ h1{
     }
     to {
         display:flex;
-        opacity:1;
+        opacity:0.9;
     }
 }
 
 @keyframes hiddenAnimation {
     from {
         display:flex;
-        opacity:1;
+        opacity:0.9;
     }
     to {
         display:none;
